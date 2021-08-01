5b2d010063e2d9222016b5ef9f8063c7b224e279
Viktor Sudarinen <salveffy@gmail.com>
2021-08-01 19:03:47 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="app">
+    <v-app-bar
+      absolute
+      color="white"
+      dense
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+      app
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title><v-icon>mdi-virus-outline</v-icon> COVID-2019</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+<v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-7"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+    </v-sheet>
+    <v-content app class="ma-2">
+    <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+</style>
