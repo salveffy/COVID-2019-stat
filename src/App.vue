6c879957d75b828398dc2b50297f2402bb4c980c
Viktor Sudarinen <salveffy@gmail.com>
2021-08-03 21:25:27 +0300

    moved the link to the logo and deleted drawer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <v-app id="app">
-    <v-navigation-drawer
-      v-model="drawer"
-      class="grey darken-4"
-      dark
-      app
-    >
-      <v-list>
-        <v-list-item v-for="item in items" :key="item.title" link :to="item.link">
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       absolute
       color="white "
@@ -26,11 +9,11 @@
       scroll-target="#scrolling-techniques-7"
       app
     >
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title class="headline"
-        ><v-icon>mdi-virus-outline</v-icon> COVID-2019</v-toolbar-title
-      >
+      <router-link class="text-d grey--text text--darken-1 ma-10" to="/">
+        <v-toolbar-title class="headline">
+          <v-icon>mdi-virus-outline</v-icon> COVID-2019</v-toolbar-title>
+      </router-link>
 
       <v-spacer></v-spacer>
       <v-menu left bottom>
@@ -70,9 +53,7 @@ export default {
   data () {
     return {
       items: [
-        { title: 'Статистика по миру', icon: 'mdi-earth', link: '/' },
-        { title: 'Статистика по стране', icon: 'mdi-flag-outline', link: '/countries' },
-        { title: 'Новости', icon: 'mdi-newspaper-variant-outline', link: '/countries' }
+        { title: 'COVID-2019', icon: 'mdi-earth', link: '/' }
       ],
       drawer: false
     }
@@ -95,5 +76,8 @@ export default {
 <style>
 #app {
   color: #2c3e50;
+}
+.text-d{
+  text-decoration: none;
 }
 </style>
